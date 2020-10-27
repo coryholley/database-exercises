4f64c790f76e37363c944b867f9d75768a8a3336
coryholley <courtneynholley@gmail.com>
2020-10-27 12:14:26 -0500

    Feat: Problem 4 completed and selected deleted

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
-SELECT '-----Albums Released After 1991-------' AS 'Info';
-SELECT * FROM albums WHERE release_date >= 1991;
+SELECT '-----Albums Released After 1991 - DELETED -------' AS 'Info';
+DELETE FROM albums WHERE release_date >= 1991;
 
-SELECT '-----Albums with Genre Disco-------' AS 'Info';
-SELECT * FROM albums WHERE genre = 'Disco';
+SELECT '-----Albums with Genre Disco - DELETED -------' AS 'Info';
+DELETE FROM albums WHERE genre = 'Disco';
 
-SELECT '-----Albums by Whitney Houston-------' AS 'Info';
-SELECT * FROM albums WHERE artist = 'Celine Dion';
+SELECT '-----Albums by Whitney Houston - DELETED -------' AS 'Info';
+DELETE FROM albums WHERE artist = 'Celine Dion';
