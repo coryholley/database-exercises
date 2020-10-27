4e3b49d647676de48ce01ac467b1b7f7516ee8af
coryholley <courtneynholley@gmail.com>
2020-10-27 11:03:13 -0500

    Feat: Problem 1 -a

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(25),
-    name  VARCHAR(25) NOT NULL,
-    release_date INT,
-    sales FLOAT,
-    genre VARCHAR(25),
+    artist VARCHAR(50),
+    name_of_album  VARCHAR(75) NOT NULL DEFAULT 'UNKNOWN',
+    release_date INT UNSIGNED,
+    genre VARCHAR(50),
+    sales FLOAT UNSIGNED,
     PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,4 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name_of_album, release_date, genre, sales)
+VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3);
