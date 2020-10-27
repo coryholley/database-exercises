41168a0fefa29508f5c35797cca353d7f18dd7f8
coryholley <courtneynholley@gmail.com>
2020-10-27 12:12:09 -0500

    Feat: Problem 4 Selects before delete

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,1 +1,10 @@
 USE codeup_test_db;
+
+SELECT '-----Albums Released After 1991-------' AS 'Info';
+SELECT * FROM albums WHERE release_date >= 1991;
+
+SELECT '-----Albums with Genre Disco-------' AS 'Info';
+SELECT * FROM albums WHERE genre = 'Disco';
+
+SELECT '-----Albums by Whitney Houston-------' AS 'Info';
+SELECT * FROM albums WHERE artist = 'Celine Dion';
