d09b6c91d90600dfd337c0ddbc8c9038bc3cfcc1
coryholley <courtneynholley@gmail.com>
2020-10-29 14:18:24 -0500

    Feat: First query of Joins Employees Database Exercise

---
+++ join_exercises.sql
@@ -0,0 +1,10 @@
+USE employees;
+
+SELECT DISTINCT title FROM TITLES;
+
+SELECT d.dept_name AS 'Department_Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department_Manager'
+FROM departments as d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01';
+
