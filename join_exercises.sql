5061138f627db6ea3653027ea13729713a3176e9
coryholley <courtneynholley@gmail.com>
2020-10-29 14:20:46 -0500

    Feat: Finished #3 of Joins/Employees Exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -8,3 +8,8 @@ FROM departments as d
          JOIN employees e on dm.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01';
 
+SELECT d.dept_name AS 'Department_Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department_Manager'
+FROM departments as d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
