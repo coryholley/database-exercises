e6df29959fa9d30bdd8c35b3dafc7506087aea1b
coryholley <courtneynholley@gmail.com>
2020-10-29 14:36:18 -0500

    Feat: Problem 4 of Joins/Employees exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -13,3 +13,15 @@ FROM departments as d
          JOIN dept_manager dm on d.dept_no = dm.dept_no
          JOIN employees e on dm.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
+
+select dept_name from departments;
+
+SELECT t.title AS Title, COUNT(e.emp_no) AS COUNT
+FROM titles as t
+         JOIN employees AS e on e.emp_no = t.emp_no
+         JOIN dept_emp AS de on de.emp_no = e.emp_no
+         JOIN departments AS d on d.dept_no = de.dept_no
+WHERE t.to_date = '9999-01-01'
+AND de.to_date = '9999-01-01'
+AND d.dept_name = 'Customer Service'
+GROUP BY t.title;
