98cb4db843afc09b9a104254372cae0007d08fe8
coryholley <courtneynholley@gmail.com>
2020-10-30 10:56:10 -0500

    Feat: problem 2 of subqueries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,12 +7,19 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date IN (
     SELECT hire_date
-    FROM employees WHERE emp_no = '101010'
+    FROM employees
+    WHERE emp_no = '101010'
 );
 
 #
 # Find all the titles held by all employees with the first name Aamod.
-#
 # 314 total titles, 6 unique titles
+SELECT title, COUNT(title)
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod')
+GROUP BY title;
 #
 # Find all the current department managers that are female.
